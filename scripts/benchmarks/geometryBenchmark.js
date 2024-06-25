23d6da5c094c3cfeb8257106246a5dda7959e55c
m4drian <m4drian@gmail.com>
2024-06-25 02:04:20 +0200

    added fps counter

--- scripts/benchmarks/geometryBenchmark.js
+++ scripts/benchmarks/geometryBenchmark.js
@@ -1,8 +1,23 @@
 // Import necessary modules
-import * as THREE from 'three';
-import * as WebGPU from 'three/addons/capabilities/WebGPU.js';
 import WebGPURenderer from 'three/addons/renderers/webgpu/WebGPURenderer.js';
-import { getRandomBaseColor, getMonochromaticColor } from '../colorScheme.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { getMonochromaticColor, getRandomBaseColor } from '../colorScheme.js';
+import * as THREE from 'three';
+
+var stats0 = new Stats();
+stats0.showPanel( 0 );
+stats0.domElement.style.cssText = 'position:fixed;bottom:20px;left:0px;';
+document.body.appendChild( stats0.dom );
+
+var stats1 = new Stats();
+stats1.showPanel( 1 );
+stats1.domElement.style.cssText = 'position:fixed;bottom:20px;left:80px;';
+document.body.appendChild( stats1.dom );
+
+var stats2 = new Stats();
+stats2.showPanel( 2 );
+stats2.domElement.style.cssText = 'position:fixed;bottom:20px;left:160px;';
+document.body.appendChild( stats2.dom );
 
 // Function to set up the scene
 function setupScene() {
@@ -15,7 +30,7 @@ function setupScene() {
 // Function to set up the camera
 function setupCamera() {
     // Set up the camera
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, 16 / 9, 0.1, 1000);
     camera.position.set(1, 1, 100);
     camera.lookAt(0, 0, 0);
     return camera;
@@ -26,7 +41,7 @@ function setupWebGLRenderer() {
     // Set up the renderer
     const renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( 2560, 1440 );//window.innerWidth, window.innerHeight);
+    renderer.setSize( 1600, 900 );//window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     return renderer;
 }
@@ -37,7 +52,7 @@ function setupWebGPURenderer() {
     // Set up the renderer
     const renderer = new WebGPURenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( 2560, 1440 );
+    renderer.setSize( 1600, 900 );
     //renderer.setAnimationLoop( animate );
     document.body.appendChild( renderer.domElement );
     return renderer;
@@ -46,7 +61,7 @@ function setupWebGPURenderer() {
 // Function to create gemetric objects and add them to the scene
 function createGeometry(scene) {
     const geometry = new THREE.SphereGeometry( 1, 32, 16 );// verts:482, Edges: 992, Faces: 512, Tris: 960 //BoxGeometry(1, 1, 1);
-    const NUM_OBJECTS = 10000;
+    const NUM_OBJECTS = 6000;
     for (let i = 0; i < NUM_OBJECTS; i++) {
         const material = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, color: getMonochromaticColor(getRandomBaseColor(), 50) });
         const cubeMesh = new THREE.Mesh(geometry, material);
@@ -59,37 +74,43 @@ function createGeometry(scene) {
 
 // Function to animate the scene
 function animate(scene, camera, renderer, rendererType) {
-    function render() {
+    async function render() {
         requestAnimationFrame(render);
 
         if (rendererType === 'webgl')
         {
-            renderer.clear();
+            await renderer.clear();
         } else {
-            renderer.clearAsync();
+            await renderer.clearAsync();
         }
 
         // Rotate cubes
-        scene.traverse((object) => {
+        /*scene.traverse((object) => {
             if (object instanceof THREE.Mesh) {
                 object.rotation.x += 0.001;
                 object.rotation.y += 0.01;
             }
-        });
+        });*/
 
         // Render scene
         if (rendererType === 'webgl')
         {
-            renderer.render(scene, camera);
+            await renderer.render(scene, camera);
         } else {
-            renderer.renderAsync(scene, camera);
+            await renderer.renderAsync(scene, camera);
         }
+
+        stats0.update();
+        stats1.update();
+        stats2.update();
     }
+    
     render();
 }
 
 // Function to run the benchmark
 export function loadGeometryBenchmark(rendererType) {
+
     const scene = setupScene();
     const camera = setupCamera();
     let renderer = null
